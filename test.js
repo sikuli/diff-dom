239d2623da262eb8a74cdd1a4d98a3cce0ea788c
Emily Bertelson <emily.bertelson@gmail.com>
2015-02-05 17:50:43 -0700

    by-line diff of html files output to new file, with changes wrapped in divs; close #4

--- test.js
+++ test.js
@@ -2,8 +2,10 @@ var dom = require('./lib/diffdom')
 var fs = require('fs')
 
 // TODO: more robust command line parsing
-var html1 = fs.readFileSync(process.argv[2], 'utf8')
-var html2 = fs.readFileSync(process.argv[3], 'utf8')
+// var html1 = fs.readFileSync(process.argv[2], 'utf8')
+// var html2 = fs.readFileSync(process.argv[3], 'utf8')
 
-dom.diff(html1, html2)
-dom.pairedDiff(html1, html2)
+// dom.diff(html1, html2)
+// dom.pairedDiff(html1, html2)
+
+dom.newDiff(process.argv[2], process.argv[3], process.argv[4])
